57ad3fd38a8bd6e94e677b5ab7abbf5a65882a11
awehrer <anthony.wehrer@gmail.com>
2022-10-29 22:27:33 -0400

    Update doppelTextConversion.js

--- doppelTextConversion.js
+++ doppelTextConversion.js
@@ -9,14 +9,14 @@ function convertDoppelText()
 	
 	if (effectsArray[0].includes("Damage") && (effectsArray[0].includes("Enemy") || effectsArray[0].includes("Enemies")))
 	{
-		temp = effectsArray[0].split("[");
+		temp = effectsArray[0].split(" [");
 		resultText += "| Magia2 effect 1 = " + temp[0] + "\n| Magia2 1 = " + temp[1].replace("]", "") + "\n";
 		index++;
 	}
 	
 	while (index < effectsArray.length)
 	{
-		temp = effectsArray[0].split("(");
+		temp = effectsArray[0].split(" (");
 		resultText += "| Magia2 effect " + (index + 1) + " = " + temp[0] + "\n| Magia2 " + (index + 1) + " = " + temp[1].replace(")", "") + "\n";
 		index++;
 	}
