5d8bbf095955ac9f2ba95a9d2b05021a8e587540
awehrer <anthony.wehrer@gmail.com>
2022-10-29 22:18:33 -0400

    Initial commit

---
+++ doppelTextConversion.js
@@ -0,0 +1,25 @@
+function convertDoppelText()
+{
+    var doppelEffectString = document.getElementById("doppelText").value;
+    var effectsArray = doppelEffectString.split(" & ");
+	var temp;
+	var index = 0;
+	
+	var resultText = "";
+	
+	if (effectsArray[0].includes("Damage") && (effectsArray[0].includes("Enemy") || effectsArray[0].includes("Enemies"))
+	{
+		temp = effectsArray[0].split("[");
+		resultText += "| Magia2 effect 1 = " + temp[0] + "\n| Magia2 1 = " + temp[1].replace("]", "") + "\n";
+		index++;
+	}
+	
+	while (index < effectsArray.length)
+	{
+		temp = effectsArray[0].split("(");
+		resultText += "| Magia2 effect " + (index + 1) + " = " + temp[0] + "\n| Magia2 " + (index + 1) + " = " + temp[1].replace(")", "") + "\n";
+		index++;
+	}
+	
+	return resultText;
+}
